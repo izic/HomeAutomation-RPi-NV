db4fecedb51df66b36438971c1e3da21dd06f9f5
Ivo Zic <izic@mail.inet.hr>
2016-06-29 14:38:39 +0200

    Commit

---
+++ HomeAutomation/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+
+using System.ServiceModel;
+
+namespace HomeAutomation.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index ()
+		{
+			var mvcName = typeof(Controller).Assembly.GetName ();
+			var isMono = Type.GetType ("Mono.Runtime") != null;
+
+			ViewData ["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+			ViewData ["Runtime"] = isMono ? "Mono_Ivina" : ".NET";
+
+
+			return View ();
+		}
+
+		public ActionResult TestButton ()
+		{
+
+			string name = "";
+			var binding = new BasicHttpBinding ();
+			var address = new EndpointAddress ("http://localhost:8090");
+			var client = new Client.MyServiceClient (binding, address);
+
+			client.Greet (name);
+
+			return View ();
+		}
+	}
+}
+
